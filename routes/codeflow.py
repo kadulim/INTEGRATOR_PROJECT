fc2b03e6cc54c740a0c8f953bf183515a2989482
kadulim <kadu.carloslima@gmail.com>
2026-06-14 21:24:04 -0300

    fix: codeflow.py missing import for current_user

--- routes/codeflow.py
+++ routes/codeflow.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, current_app
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 codeflow_bp = Blueprint('codeflow', __name__, url_prefix='/codeflow')
 
